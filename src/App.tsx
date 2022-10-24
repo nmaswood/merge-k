c02aa0d5f93e693a401ffdea017903fb54cc92a2
Nasr Maswood <nasr@labelbox.com>
2022-10-23 22:09:38 -0400

    blow away old code

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,7 @@
-import { StockPriceExplorer } from "./stock-price-explorer/stock-price-explorer";
 import "./App.css";
 
-const TEST_DATES = ["1/17/2014", "1/21/2014", "1/22/2014"];
-const TEST_PRICES: Record<string, (number | null)[]> = {
-  AAPL: [19.31, 19.61, 19.7],
-  MSFT: [36.38, null, 35.93],
-  ARIEL: [100, null, 2000],
-  NASR: [1, 2, 3],
-  TECH: [4, 5, 6],
-};
 function App() {
-  return (
-    <div className="App">
-      <StockPriceExplorer dates={TEST_DATES} prices={TEST_PRICES} />
-    </div>
-  );
+  return <div className="App">Hello World</div>;
 }
 
 export default App;
--- src/stock-price-explorer/parse-date.ts
+++
@@ -1,6 +0,0 @@
-export function parseDate(d: string): Date {
-  var [month, day, year] = d.split("/").map((n) => parseInt(n, 10));
-
-  // month index gotcha
-  return new Date(year, month - 1, day);
-}
